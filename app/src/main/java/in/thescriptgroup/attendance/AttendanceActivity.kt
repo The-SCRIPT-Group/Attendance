9798547abb731f9b149fb005a72d006e3c0b8189
Akhil Narang <akhilnarang.1999@gmail.com>
2019-11-26 23:58:58 +0530

    Add a progressbar, shown after clicking the newly added button, until attendance is fetched
    Need to change the button into something sane
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app/src/main/java/in/thescriptgroup/attendance/AttendanceActivity.kt
+++ app/src/main/java/in/thescriptgroup/attendance/AttendanceActivity.kt
@@ -3,6 +3,7 @@ package `in`.thescriptgroup.attendance
 import `in`.thescriptgroup.attendance.models.Subject
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_attendance.*
@@ -17,26 +18,44 @@ class AttendanceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attendance)
         title = "Attendance"
-        val username = intent.getStringExtra("username")!!
-        val password = intent.getStringExtra("password")!!
-        val call = ApiClient.client.create(Attendance::class.java).getAttendance(username, password)
-        call.enqueue(object : Callback<List<Subject>> {
-            override fun onResponse(call: Call<List<Subject>>, response: Response<List<Subject>>) {
-                Objects.requireNonNull<List<Subject>>(response.body(), "Response body is null")
-                val attendanceData: List<Subject> = response.body()!!
-                attendanceData.forEach {
-                    try {
-                        attendanceView.append("${it.type} - ${it.name} \t ${String.format("%.2f", (it.present / it.total.toDouble()) * 100)}%\n")
-                    } catch (e: ArithmeticException) {
+        Toast.makeText(this, "Click on the button to check attendance!", Toast.LENGTH_SHORT).show()
+        progress.visibility = View.GONE
+        attendanceButton.setOnClickListener {
+            progress.visibility = View.VISIBLE
+            val username = intent.getStringExtra("username")!!
+            val password = intent.getStringExtra("password")!!
+            val call =
+                ApiClient.client.create(Attendance::class.java).getAttendance(username, password)
+
+
+            call.enqueue(object : Callback<List<Subject>> {
+                override fun onResponse(
+                    call: Call<List<Subject>>,
+                    response: Response<List<Subject>>
+                ) {
+                    Objects.requireNonNull<List<Subject>>(response.body(), "Response body is null")
+                    val attendanceData: List<Subject> = response.body()!!
+                    progress.visibility = View.GONE
+
+                    attendanceData.forEach {
+                        try {
+                            attendanceView.append(
+                                "${it.type} - ${it.name} \t ${String.format(
+                                    "%.2f",
+                                    (it.present / it.total.toDouble()) * 100
+                                )}%\n"
+                            )
+                        } catch (e: ArithmeticException) {
+                        }
                     }
                 }
-            }
 
-            override fun onFailure(call: Call<List<Subject>>, t: Throwable) {
-                Log.v("onFailure", t.message!!)
-                Toast.makeText(this@AttendanceActivity, "Error occurred!", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        })
+                override fun onFailure(call: Call<List<Subject>>, t: Throwable) {
+                    Log.v("onFailure", t.message!!)
+                    Toast.makeText(this@AttendanceActivity, "Error occurred!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+        }
     }
 }
