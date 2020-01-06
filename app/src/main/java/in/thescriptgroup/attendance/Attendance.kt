d00089faadd8d372a82c3e536db9377e1da3a9ff
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-06 07:42:23 +0530

    Attendance: make app slightly harder to decompile
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app/src/main/java/in/thescriptgroup/attendance/Attendance.kt
+++ app/src/main/java/in/thescriptgroup/attendance/Attendance.kt
@@ -11,14 +11,49 @@ import retrofit2.http.POST
 import java.util.concurrent.TimeUnit
 
 object ApiClient {
-    private const val BASE_URL = "https://poseidon.thescriptgroup.in/api/"
+    private val BASE_URL = arrayOf(
+        "h",
+        "t",
+        "t",
+        "p",
+        "s",
+        ":",
+        "/",
+        "/",
+        "p",
+        "o",
+        "s",
+        "e",
+        "i",
+        "d",
+        "o",
+        "n",
+        ".",
+        "t",
+        "h",
+        "e",
+        "s",
+        "c",
+        "r",
+        "i",
+        "p",
+        "t",
+        "g",
+        "r",
+        "o",
+        "u",
+        "p",
+        ".",
+        "i",
+        "n"
+    )
     private var retrofit: Retrofit? = null
 
     val client: Retrofit
         get() {
             if (retrofit == null) {
                 retrofit = Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(BASE_URL.toString())
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS).build())
                     .build()
