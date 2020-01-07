43577953d8a201d14656a826bd25b236403891f0
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-07 10:46:56 +0530

    Attendance: check credentials on login
      * Functionality untested, only compile tested
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app/src/main/java/in/thescriptgroup/attendance/LoginActivity.kt
+++ app/src/main/java/in/thescriptgroup/attendance/LoginActivity.kt
@@ -1,13 +1,22 @@
 package `in`.thescriptgroup.attendance
 
+import `in`.thescriptgroup.attendance.models.Subject
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,23 +26,62 @@ class LoginActivity : AppCompatActivity() {
             getString(R.string.preference_file_key), Context.MODE_PRIVATE
         )
         submit.setOnClickListener {
-            val username = username.text.toString()
-            val password = password.text.toString()
+            val username = username_input.text.toString()
+            val password = password_input.text.toString()
 
             if (username.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Please enter the details!", Toast.LENGTH_SHORT).show()
             } else {
-                with(sharedPref.edit()) {
-                    putString(getString(R.string.username_key), username)
-                    putString(getString(R.string.password_key), password)
-                    commit()
-                }
-                val intent = Intent(this, AttendanceActivity::class.java)
-                intent.putExtra("username", username)
-                intent.putExtra("password", password)
-                finish()
-                startActivity(intent)
+                val call =
+                    ApiClient.client.create(Attendance::class.java)
+                        .getAttendance(username, password)
+                call.enqueue(object : Callback<List<Subject>> {
+                    override fun onResponse(
+                        call: Call<List<Subject>>,
+                        response: Response<List<Subject>>
+                    ) {
+                        Objects.requireNonNull<List<Subject>>(
+                            response.body(),
+                            "Response body is null"
+                        )
+                        val attendanceData: List<Subject> = response.body()!!
+                        val err: String? = attendanceData[0].response
+                        if (err != null) {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                err,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            password_input.text.clear()
+                        } else {
+                            val attendanceStr = gson.toJson(attendanceData)
+                            val timestamp = Calendar.getInstance().time.toString()
+                            with(sharedPref.edit()) {
+                                putString(getString(R.string.attendance_key), attendanceStr)
+                                putString(getString(R.string.timestamp_key), timestamp)
+                                commit()
+                            }
+                            val intent = Intent(this@LoginActivity, AttendanceActivity::class.java)
+                            intent.putExtra("username", username)
+                            intent.putExtra("password", password)
+                            finish()
+                            startActivity(intent)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<List<Subject>>, t: Throwable) {
+                        Log.v("onFailure", t.message!!)
+                        if (t.message == "timeout") {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Connection timed out!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                })
             }
         }
     }
 }
+
