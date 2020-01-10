f758b5fee6d9c973195ae72acebd1b8a71b96548
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-10 11:23:47 +0530

    Attendance: remove actionbar from login page, delete "newlogin", add imageview to login
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app/src/main/java/in/thescriptgroup/attendance/LoginActivity.kt
+++ app/src/main/java/in/thescriptgroup/attendance/LoginActivity.kt
@@ -22,7 +22,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_newlogin)
+        setContentView(R.layout.activity_login)
 
         val sharedPref = this.getSharedPreferences(
             getString(R.string.preference_file_key), Context.MODE_PRIVATE
