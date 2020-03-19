1d4bd3e415280a27abb4ced597a8c0a612086976
Aditya Desai <aditya.desai0199@gmail.com>
2020-03-19 18:56:12 +0530

    Reorganize code
    Signed-off-by: Aditya Desai <aditya.desai0199@gmail.com>

--- app/src/main/java/in/thescriptgroup/attendance/SubjectViewHolder.kt
+++ app/src/main/java/in/thescriptgroup/attendance/SubjectViewHolder.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
@@ -56,59 +57,63 @@ class ListAdapter(private val list: ArrayList<Subject>) :
         return SubjectViewHolder(inflater, parent)
     }
 
+    private fun getSubjectDetails(view: View, subject: Subject): String {
+        var message = ""
+
+        if (subject.name == "Total") {
+            val (total_present, total_total) = subject.getTotal()
+            message += "Total Attendance:  $total_present / $total_total ( ${String.format(
+                "%.2f",
+                (total_present / total_total.toDouble()) * 100
+            )}% )\n\n"
+        }
+
+        message += "Attended :-\n"
+
+        if (subject.th_total != 0) {
+            message += "\t\tTheory: ${subject.th_present} / ${subject.th_total} ( ${String.format(
+                "%.2f",
+                (subject.th_present / subject.th_total.toDouble()) * 100
+            )}% )\n"
+        }
+        if (subject.pr_total != 0) {
+            message += "\t\tPractical: ${subject.pr_present} / ${subject.pr_total} ( ${String.format(
+                "%.2f",
+                (subject.pr_present / subject.pr_total.toDouble()) * 100
+            )}% )\n"
+        }
+        if (subject.tu_total != 0) {
+            message += "\t\tTutorial: ${subject.tu_present} / ${subject.tu_total} ( ${String.format(
+                "%.2f",
+                (subject.tu_present / subject.tu_total.toDouble()) * 100
+            )}% )\n"
+        }
+        message += "\n\n"
+
+        val sharedPref: SharedPreferences = view.context.getSharedPreferences(
+            view.context.getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        )
+        val desired =
+            sharedPref.getInt(view.context.getString(R.string.desired_attendance_key), 75)
+        val data = subject.calculateLectures(desired)
+        for (key in data.keys) {
+            message += "You " +
+                    when {
+                        data[key]!! < 0 -> "need to attend ${abs(data[key]!!)}"
+                        data[key]!! > 0 -> "can bunk ${data[key]}"
+                        else -> "cannot bunk any"
+                    } + " $key \n"
+        }
+        return message
+    }
+
     override fun onBindViewHolder(holder: SubjectViewHolder, position: Int) {
         holder.bind(list[position])
 
         holder.itemView.setOnClickListener { view ->
             val subject = list[position]
             val customDialog = AlertDialog.Builder(view.context)
-            var message = ""
-
-            if (subject.name == "Total") {
-                val (total_present, total_total) = subject.getTotal()
-                message += "Total Attendance:  $total_present / $total_total ( ${String.format(
-                    "%.2f",
-                    (total_present / total_total.toDouble()) * 100
-                )}% )\n\n"
-            }
-
-            message += "Attended :-\n"
-
-            if (subject.th_total != 0) {
-                message += "\t\tTheory: ${subject.th_present} / ${subject.th_total} ( ${String.format(
-                    "%.2f",
-                    (subject.th_present / subject.th_total.toDouble()) * 100
-                )}% )\n"
-            }
-            if (subject.pr_total != 0) {
-                message += "\t\tPractical: ${subject.pr_present} / ${subject.pr_total} ( ${String.format(
-                    "%.2f",
-                    (subject.pr_present / subject.pr_total.toDouble()) * 100
-                )}% )\n"
-            }
-            if (subject.tu_total != 0) {
-                message += "\t\tTutorial: ${subject.tu_present} / ${subject.tu_total} ( ${String.format(
-                    "%.2f",
-                    (subject.tu_present / subject.tu_total.toDouble()) * 100
-                )}% )\n"
-            }
-            message += "\n\n"
-            val sharedPref: SharedPreferences = view.context.getSharedPreferences(
-                view.context.getString(R.string.preference_file_key), Context.MODE_PRIVATE
-            )
-
-            val desired =
-                sharedPref.getInt(view.context.getString(R.string.desired_attendance_key), 75)
-            val data = subject.calculateLectures(desired)
-            for (key in data.keys) {
-                message += "You " +
-                        when {
-                            data[key]!! < 0 -> "need to attend ${abs(data[key]!!)}"
-                            data[key]!! > 0 -> "can bunk ${data[key]}"
-                            else -> "cannot bunk any"
-                        } + " $key \n"
-            }
-
+            val message = getSubjectDetails(view, subject)
             customDialog
                 .setMessage(message)
                 .setNeutralButton("Dismiss") { dialog, _ ->
